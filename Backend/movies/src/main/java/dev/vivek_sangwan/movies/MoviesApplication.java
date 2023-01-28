e309899c218881db3ddaec5d43f57c22d7b984bc
viveksangwan-cmd <viveksangwan5791@gmail.com>
2023-01-28 22:52:03 +0530

    Cross-Origin-Access
    Enabled Cross-Origin-Access and GetAllMovies on Frontend

--- Backend/movies/src/main/java/dev/vivek_sangwan/movies/MoviesApplication.java
+++ Backend/movies/src/main/java/dev/vivek_sangwan/movies/MoviesApplication.java
@@ -2,6 +2,7 @@ package dev.vivek_sangwan.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+	@CrossOrigin
 	@GetMapping("healthcheck")
 	public String check(){
 		return "Backend is running fine";
--- Backend/movies/src/main/java/dev/vivek_sangwan/movies/ReviewController.java
+++ Backend/movies/src/main/java/dev/vivek_sangwan/movies/ReviewController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,6 +13,7 @@ import java.util.Map;
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
+    @CrossOrigin
     @PostMapping
     public ResponseEntity<Review> addNewReview(@RequestBody Map<String,String> payload){
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")), HttpStatus.CREATED);
